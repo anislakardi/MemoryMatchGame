a5ec76de28f82028835fed147a426bf7ec307dcd
Lakardi Mohamed Anis <127695733+lakardi-mohamed-anis@users.noreply.github.com>
2024-12-06 15:50:00 +0100

    version finale 2.2

--- VERSION FINALE/src/memorycartes/Fonctions.java
+++ VERSION FINALE/src/memorycartes/Fonctions.java
@@ -2,15 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pdd;
+package memorycartes;
 import java.util.*;
 import javax.swing.JButton;
+
 /**
  *
  * @author PC
  */
 public class Fonctions {
-    
     
     public static void setAllButtonsVisible(JButton[] buttons,boolean b ) {
         for (JButton button : buttons) {
@@ -18,23 +18,22 @@ public class Fonctions {
         }
     }
 
-    // Method to shuffle an array of colors (or any array)
     public static String[] shuffleArray(String[] table) {
-        List<String> list = new ArrayList<>(Arrays.asList(table)); // Convert array to a List
-        Collections.shuffle(list);  // Shuffle the list
-        return list.toArray(new String[0]);  // Convert the list back to an array
+        List<String> list = new ArrayList<>(Arrays.asList(table));
+        Collections.shuffle(list);
+        return list.toArray(new String[0]);
     }
     
     public static JButton[] shuffleButtons(JButton[] buttons) {
         Random random = new Random();
         for (int k = buttons.length - 1; k > 0; k--) {
-            int j = random.nextInt(k + 1); // Génère un index aléatoire entre 0 et i
-            // Échange des éléments
+            int j = random.nextInt(k + 1);
+            
             JButton temp = buttons[k];
             buttons[k] = buttons[j];
             buttons[j] = temp;
         }
-        return buttons; // Retourne le tableau mélangé
+        return buttons;
     }
     
 }
